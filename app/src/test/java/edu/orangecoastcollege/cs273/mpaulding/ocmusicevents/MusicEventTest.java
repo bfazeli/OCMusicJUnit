692a6bf774347043ee11d45f8be193c6211b44d1
bijan102 <bfazeli1@gmail.com>
2016-11-22 14:51:30 -0800

    Created a JUnit test file for MusicEvent.

---
+++ app/src/test/java/edu/orangecoastcollege/cs273/mpaulding/ocmusicevents/MusicEventTest.java
@@ -0,0 +1,103 @@
+package edu.orangecoastcollege.cs273.mpaulding.ocmusicevents;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bijanfazeli on 11/22/16.
+ */
+public class MusicEventTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getTitle() throws Exception {
+
+    }
+
+    @Test
+    public void setTitle() throws Exception {
+
+    }
+
+    @Test
+    public void getDate() throws Exception {
+
+    }
+
+    @Test
+    public void setDate() throws Exception {
+
+    }
+
+    @Test
+    public void getDay() throws Exception {
+
+    }
+
+    @Test
+    public void setDay() throws Exception {
+
+    }
+
+    @Test
+    public void getTime() throws Exception {
+
+    }
+
+    @Test
+    public void setTime() throws Exception {
+
+    }
+
+    @Test
+    public void getLocation() throws Exception {
+
+    }
+
+    @Test
+    public void setLocation() throws Exception {
+
+    }
+
+    @Test
+    public void getAddress1() throws Exception {
+
+    }
+
+    @Test
+    public void setAddress1() throws Exception {
+
+    }
+
+    @Test
+    public void getAddress2() throws Exception {
+
+    }
+
+    @Test
+    public void setAddress2() throws Exception {
+
+    }
+
+    @Test
+    public void getImageName() throws Exception {
+
+    }
+
+    @Test
+    public void setImageName() throws Exception {
+
+    }
+
+}
